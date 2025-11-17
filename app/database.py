c81584e3cf1f3b555035a041dadb76201fad08fd
menneli <131284866+menneli@users.noreply.github.com>
2025-11-17 23:25:47 +0300

    Система прогресса и статистики и документация

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'project.db')}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
