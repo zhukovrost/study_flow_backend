62dca96bfb2e46a95c2856c0e0fc184c2959b916
menneli <131284866+menneli@users.noreply.github.com>
2025-11-23 19:43:10 +0300

    improvements

--- app/database.py
+++ app/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
+
+from app.database.base import Base
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'studyflow.db')}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
